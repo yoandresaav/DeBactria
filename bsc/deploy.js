26bc8efd63063817cb89caabd64e5937eee29dbc
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2021-02-17 20:52:48 -0600

    agregar env

--- bsc/deploy.js
+++ bsc/deploy.js
@@ -1,28 +1,16 @@
 const Web3 = require('web3');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const factory = require("../kickstart-frontend/src/build/CampaignFactory.json");
-
-var privateKeys = [
-  "e6f3923573935fc2842f090de8e91ef62c33fe982f1ad4d0f65a819d9599e634",
-];
+const config = require('./config.js');
 
 
-// Provider Ganache
-const providerGanache = new HDWalletProvider(
-  privateKeys,
-  'http://localhost:7545',
+const privateKey = [config.PRIVATE_KEY];
+const provider = new HDWalletProvider(
+  privateKey,
+  config.HOST
 );
 
-
-const privateKeysBSC = [
-  '8dcba2a5ceb090c59d712cf2952859feec4399f0e7ff32b68b4a7b7f37434014'
-];
-const providerBSC = new HDWalletProvider(
-  privateKeysBSC,
-  'https://apis.ankr.com/60a2b6d260f6408f8c893e43fe60861d/436c4191c3a955c822236c8f1a306d98/binance/full/test',
-);
-
-const web3 = new Web3(providerBSC); // Change provider this
+const web3 = new Web3(provider); // Change provider this
 let accounts;
 async function deploy(){
     accounts = await web3.eth.getAccounts();
