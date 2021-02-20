2cf8bf25173266b88f25a895faa3204b7f33930f
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2021-02-19 23:02:21 -0600

    Updates

---
+++ precache-manifest.a383d81e5b11aaa7a8570bc93152d4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df61b52f9be583d1b6f6103a2029d0ba",
+    "url": "/DeBactria/index.html"
+  },
+  {
+    "revision": "a913fc5e3655e31e7f85",
+    "url": "/DeBactria/static/css/main.7d52aada.chunk.css"
+  },
+  {
+    "revision": "3f0f5b8bb8f7c2c141ef",
+    "url": "/DeBactria/static/js/2.cd20d84a.chunk.js"
+  },
+  {
+    "revision": "7d9a1e97b63fb04c3f7226116d9275de",
+    "url": "/DeBactria/static/js/2.cd20d84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a913fc5e3655e31e7f85",
+    "url": "/DeBactria/static/js/main.f42f94a0.chunk.js"
+  },
+  {
+    "revision": "8719d20849974c3a0335",
+    "url": "/DeBactria/static/js/runtime-main.2c4b280e.js"
+  }
+]);
